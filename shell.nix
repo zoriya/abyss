169f8e502efeb1a4f68354468c1201c51eccf8b7
Zoe Roux <zoe.roux@zoriya.dev>
2025-08-30 19:22:44 +0200

    Use ergogen and keymap-drawer from nixpkgs now that they are packaged

--- shell.nix
+++ shell.nix
@@ -1,16 +1,13 @@
-{pkgs ? import <nixpkgs> {}}: let
-  keymap-drawer =
-    pkgs.python3Packages.callPackage ./draw {};
-in
-  pkgs.mkShell {
-    packages = with pkgs; [
-      kicad
-      nodejs
-      freecad
-      # keymap-drawer
-    ];
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  packages = with pkgs; [
+    kicad
+    freecad
+    ergogen
+    # keymap-drawer
+  ];
 
-    # no clue why we need those env vars for freecad
-    # QT_QPA_PLATFORM = "wayland;xcb";
-    # QT_QPA_PLATFORMTHEME = "qt5ct";
-  }
+  # no clue why we need those env vars for freecad
+  # QT_QPA_PLATFORM = "wayland;xcb";
+  # QT_QPA_PLATFORMTHEME = "qt5ct";
+}
